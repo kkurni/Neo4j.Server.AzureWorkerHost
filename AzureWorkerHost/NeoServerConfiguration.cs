11acd86eff18aac02b0fca6177d914137657608d
kkurni <kkurni@gmail.com>
2012-12-27 16:11:22 +1100

    change neo blob with the stable version

--- AzureWorkerHost/NeoServerConfiguration.cs
+++ AzureWorkerHost/NeoServerConfiguration.cs
@@ -19,13 +19,13 @@ namespace Neo4j.Server.AzureWorkerHost
         /// </summary>
         public string JavaHomeRelativePath = @"jre7\";
 
-        public string NeoBlobName = "neo4j/neo4j-community-1.8.RC1-windows.zip";
+        public string NeoBlobName = "neo4j/neo4j-community-1.8.1-windows.zip";
         public string NeoDirectoryName = "neo4j-inst";
         public string NeoEndpointId = "Neo4j";
 
         /// <summary>
         /// Folder path relative to <see cref="NeoDirectoryName"/> that corresponds to the root of the Neo distribution (must have child folders like bin and conf)
         /// </summary>
-        public string NeoBasePath = @"neo4j-community-1.8.RC1";
+        public string NeoBasePath = @"neo4j-community-1.8.1";
     }
 }
